918d68684442354162761554419438ed15dfd629
Fred Han <xuefei1@ualberta.ca>
2017-01-12 23:20:06 -0700

    first working version

--- sketch_jan10a/sketch_jan10a.ino
+++ sketch_jan10a/sketch_jan10a.ino
@@ -15,22 +15,21 @@ byte CMD_SET_LEVEL = 0x01;
 void setup() {
   // put your setup code here, to run once:
   BLEMini_begin(57600);
-  //Serial.begin(57600);
+  Serial.begin(57600);
   pinMode(DIGITAL_OUT_PIN_1, OUTPUT);
   pinMode(DIGITAL_OUT_PIN_2, OUTPUT);
   pinMode(DIGITAL_OUT_PIN_3, OUTPUT);
   pinMode(DIGITAL_OUT_PIN_4, OUTPUT);
   pinMode(DIGITAL_OUT_PIN_5, OUTPUT);
-  //Serial.println("Init success");
+  Serial.println("Init success");
   digitalWrite(DIGITAL_OUT_PIN_1, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while ( BLEMini_available() == 5 )
+  while ( BLEMini_available() == 5)
   {
-    digitalWrite(DIGITAL_OUT_PIN_2, HIGH);
-    //Serial.println("Received something");
+    Serial.println("Received something");
     // read out command and data
     byte cmd_type = BLEMini_read();
     byte cmd = BLEMini_read();
@@ -38,7 +37,6 @@ void loop() {
     byte data1 = BLEMini_read();
     byte data2 = BLEMini_read();
     if(cmd == CMD_SET_LEVEL){
-      //Serial.println("Received data0");
       updatePos((int)data0);
     }
   }
